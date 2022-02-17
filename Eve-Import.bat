c63b780551f9abfc875ba833f41a7b485dcd18e3
keawepoo <AceSG1@gmail.com>
2022-02-16 14:15:35 -1000

    Delete Eve-Import.bat

--- Eve-Import.bat
+++
@@ -1,5 +0,0 @@
-ROBOCOPY %userprofile%\Desktop\EVE-Backup\CCP\EVE\c_eve_sharedcache_tq_tranquility %userprofile%\AppData\Local\CCP\EVE\c_eve_sharedcache_tq_tranquility /E
-
-ROBOCOPY %userprofile%\Desktop\EVE-Backup\mumble %APPDATA%\Mumble /E
-
-reg IMPORT %userprofile%\Desktop\EVE-Backup\mumble.reg
